bbd9288c40fd793d6e6bc985338e1c60a097ad31
Rafael Briet <rafael-briet@hotmail.com>
2024-09-08 14:49:24 -0300

    Cria usuário

--- api/bd_api_equiztech.sql
+++ api/bd_api_equiztech.sql
@@ -6,3 +6,32 @@ CREATE TABLE categorias (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL UNIQUE
 );
+
+CREATE TABLE nivel_acesso(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+INSERT INTO nivel_acesso (nome) VALUES ('Administrador');
+INSERT INTO nivel_acesso (nome) VALUES ('Jogador');
+
+CREATE TABLE dados_pessoais(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    sobrenome VARCHAR(255) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    biografia VARCHAR(2000),
+    nome_foto VARCHAR(255)
+);
+
+CREATE TABLE usuarios(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    criado_em DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    termos_condicoes CHAR(1) NOT NULL,
+    id_dados_pessoais INT NOT NULL,
+    id_nivel_acesso INT NOT NULL,
+    FOREIGN KEY (id_dados_pessoais) REFERENCES dados_pessoais(id),
+    FOREIGN KEY (id_nivel_acesso) REFERENCES nivel_acesso(id)
+);
---
+++ api/usuarios/index.php
@@ -0,0 +1,101 @@
+<?php
+
+require_once('../../dbconnection.php');
+
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        $response = create_user();
+        break;
+    default:
+        $response = [
+            'erro' => [ 'mensagem' => 'Método HTTP não suportado.' ]
+        ];
+        break;
+}
+
+function create_user() {
+    $request_body = file_get_contents('php://input');
+    $data = json_decode($request_body, true);
+    $user_name = $data['dados_pessoais']['nome'];
+    $user_surname = $data['dados_pessoais']['sobrenome'];
+    $user_birthday = $data['dados_pessoais']['data_nascimento'];
+    $user_birthday_date = date("Y-m-d", strtotime($user_birthday));
+    $user_bio = $data['dados_pessoais']['biografia'];
+    $user_photo = $data['dados_pessoais']['nome_foto'];
+    $user_email = $data['usuario']['email'];
+    $user_password = $data['usuario']['senha'];
+    $user_password_hashed = password_hash($user_password, PASSWORD_BCRYPT);
+    $user_conditions = $data['usuario']['termos_condicoes'];
+    $user_access_level_id = $data['usuario']['id_nivel_acesso']; 
+    $response = [];
+
+    try {
+        $connection = create_connection();
+        $user_already_exist = get_user_by_email($user_email);
+        
+        if ($user_already_exist) {
+            $response = [
+                'erro' => [ 'mensagem' => 'Já existe um usuário cadastrada com esse e-mail.' ]
+            ];
+        } else {
+            $query = $connection->prepare('INSERT INTO dados_pessoais (nome, sobrenome, data_nascimento, biografia, nome_foto) VALUES (?, ?, ?, ?, ?)');
+            $query->bind_param('sssss', $user_name, $user_surname, $user_birthday_date, $user_bio, $user_photo);
+            $query->execute();
+            $user_personal_data_id = $query->insert_id;
+
+            $query = $connection->prepare('INSERT INTO usuarios (email, senha, termos_condicoes, id_dados_pessoais, id_nivel_acesso) VALUES (?, ?, ?, ?, ?)');
+            $query->bind_param('sssii', $user_email, $user_password_hashed, $user_conditions, $user_personal_data_id, $user_access_level_id);
+            $query->execute();
+            $user_id = $query->insert_id;
+
+            $query = $connection->prepare('SELECT nome FROM nivel_acesso WHERE id = ?');
+            $query->bind_param('i', $user_access_level_id);
+            $query->execute();
+            $result = $query->get_result();
+
+            $response = [
+                'usuario' => [
+                    'id' => $user_id,
+                    'email' => $user_email,
+                    'termos_condicoes' => $user_conditions,
+                    'id_nivel_acesso' => $user_access_level_id,
+                    'nome_nivel_acesso' => $result->fetch_assoc()['nome'],
+                    'id_dados_pessoais' => $user_personal_data_id,
+                    'dados_pessoais' => [
+                        'nome' => $user_name,
+                        'sobrenome' => $user_surname,
+                        'data_nascimento' => $user_birthday_date,
+                        'biografia' => $user_bio,
+                        'nome_foto' => $user_photo,
+                    ]
+                ]
+            ];
+        }
+    } catch (\Throwable $th) {
+        $response = [
+            'erro' => [ 'mensagem' => 'Ocorreu um erro. Estamos trabalhando nisso e consertaremos em breve. Obrigado pela sua paciência!' ]
+        ];
+    }
+
+    return $response;
+}
+
+function get_user_by_email($email) {
+    try {
+        $connection = create_connection();
+        $query = $connection->prepare('SELECT id, email, criado_em, id_dados_pessoais, id_nivel_acesso FROM usuarios WHERE email = ?');
+        $query->bind_param('s', $email);
+        $query->execute();
+        $result = $query->get_result();
+        $response = $result->fetch_assoc();
+    } catch (\Throwable $th) {
+        $response = [
+            'erro' => [ 'mensagem' => 'Ocorreu um erro. Estamos trabalhando nisso e consertaremos em breve. Obrigado pela sua paciência!' ]
+        ];
+    }
+
+    return $response;
+}
+
+header('Content-Type: application/json');
+echo json_encode($response);
