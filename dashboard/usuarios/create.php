4609865d9aa8f43395cc9293a3862e95875f4501
Rafael Briet <rafael-briet@hotmail.com>
2024-09-24 20:19:15 -0300

    Adiciona a página de cadastro de usuário

---
+++ dashboard/usuarios/create.php
@@ -0,0 +1,71 @@
+<?php
+
+require('../functions.php');
+
+only_admin_allowed();
+get_header();
+get_partial('nav');
+
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <?php get_partial('sidebar'); ?>
+
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-white">
+            <div class="d-flex justify-content-between flex-wrap flex-md-no-wrap align-items-center border-bottom pt-3 pb-2 mb-3">
+                <h1 class="h2">Cadastrar usuário</h1>
+            </div>
+
+            <div class="mb-3">
+                <form class="form-dashboard">
+                    <div class="mb-3">
+                        <label for="userName" class="form-label">Nome</label>
+                        <input type="text" class="form-control" id="userName" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userSurname" class="form-label">Sobrenome</label>
+                        <input type="text" class="form-control" id="userSurname" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userAccessLevel" class="form-label">Nível de acesso</label>
+                        <select class="form-select" id="userAccessLevel">
+                            <option value="0" selected disabled hidden>Selecione um nível de acesso</option>
+                            <option value="volvo">Administrador</option>
+                            <option value="saab">Jogador</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userBio" class="form-label">Biografia</label>
+                        <textarea class="form-control" id="userBio" rows="8"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userBirthday" class="form-label">Data de nascimento</label>
+                        <input type="date" class="form-control" id="userBirthday" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userEmail" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="userEmail" required>
+                    </div>
+                    <div class="d-flex gap-2 justify-content-end">
+                        <a href="../usuarios" class="btn btn-outline-secondary">Voltar</a>
+                        <input type="submit" class="btn btn-primary" value="Cadastrar">
+                    </div>
+                </form>
+
+                <div class="toast-container position-fixed bottom-0 end-0 p-3">
+                    <div class="toast align-items-center border border-success-subtle bg-success-subtle text-success" id="form-success" role="alert" aria-live="assertive" aria-atomic="true">
+                        <div class="d-flex">
+                            <div class="toast-body">
+                                <p>Categoria criada com sucesso.</p>
+                            </div>
+                            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>
+
+<?php get_footer(); ?>
