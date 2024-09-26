0ac5604796ec769bfb359d27c50f8b18a156479c
Rafael Briet <rafael-briet@hotmail.com>
2024-09-26 18:05:36 -0300

    Adiciona página de edição de usuário

---
+++ dashboard/usuarios/edit.php
@@ -0,0 +1,93 @@
+<?php
+
+require('../functions.php');
+
+only_admin_allowed();
+get_header();
+get_partial('nav');
+
+$authorization = 'Authorization: ' . $_COOKIE['TOKEN'];
+
+$user_id = $_GET['id'];
+$url = $_ENV['BASE_URL'] . '/api/usuarios/?id=' . $user_id;
+$curl_handler = curl_init($url);
+curl_setopt($curl_handler, CURLOPT_HTTPGET, true);
+curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl_handler, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
+$response_json = curl_exec($curl_handler);
+curl_close($curl_handler);
+$response = json_decode($response_json, true);
+
+$url = $_ENV['BASE_URL'] . '/api/usuarios/nivel-acesso.php/';
+$curl_handler = curl_init($url);
+curl_setopt($curl_handler, CURLOPT_HTTPGET, true);
+curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl_handler, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
+$response_json = curl_exec($curl_handler);
+curl_close($curl_handler);
+$access_level = json_decode($response_json, true);
+
+?>
+
+<div class="container-fluid">
+    <div class="row">
+        <?php get_partial('sidebar'); ?>
+
+        <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 bg-white">
+            <div class="d-flex justify-content-between flex-wrap flex-md-no-wrap align-items-center border-bottom pt-3 pb-2 mb-3">
+                <h1 class="h2">Editar usuário</h1>
+            </div>
+
+            <div class="mb-3">
+                <form class="form-dashboard">
+                    <input type="hidden" class="form-control" id="categoryId" value="<?php echo $response['usuario']['id']; ?>">
+                    <div class="mb-3">
+                        <label for="userName" class="form-label">Nome</label>
+                        <input type="text" class="form-control" id="userName" value="<?php echo $response['usuario']['nome']; ?>" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userSurname" class="form-label">Sobrenome</label>
+                        <input type="text" class="form-control" id="userSurname" value="<?php echo $response['usuario']['sobrenome']; ?>" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userAccessLevel" class="form-label">Nível de acesso</label>
+                        <select class="form-select" id="userAccessLevel">
+                            <?php foreach ($access_level['nivel_acesso'] as $access) : ?>
+                                <option value="<?php echo $access['id']; ?>" <?php echo $access['id'] === $response['usuario']['id_nivel_acesso'] ? 'selected' : ''; ?> ><?php echo $access['nome']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userBio" class="form-label"><?php echo $response['usuario']['biografia']; ?></label>
+                        <textarea class="form-control" id="userBio" rows="8"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userBirthday" class="form-label">Data de nascimento</label>
+                        <input type="date" class="form-control" id="userBirthday" value="<?php echo $response['usuario']['data_nascimento']; ?>" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="userEmail" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="userEmail" value="<?php echo $response['usuario']['email']; ?>" required>
+                    </div>
+                    <div class="d-flex gap-2 justify-content-end">
+                        <a href="../usuarios" class="btn btn-outline-secondary">Voltar</a>
+                        <input type="submit" class="btn btn-primary" value="Cadastrar">
+                    </div>
+                </form>
+
+                <div class="toast-container position-fixed bottom-0 end-0 p-3">
+                    <div class="toast align-items-center border border-success-subtle bg-success-subtle text-success" id="form-success" role="alert" aria-live="assertive" aria-atomic="true">
+                        <div class="d-flex">
+                            <div class="toast-body">
+                                <p>Categoria criada com sucesso.</p>
+                            </div>
+                            <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+</div>
+
+<?php get_footer(); ?>
