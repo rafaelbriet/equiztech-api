1dc8ee639f4f52e3df7b72699329ce29b22f92a3
Rafael Briet <rafael-briet@hotmail.com>
2024-09-27 20:32:28 -0300

    Criar cadastro de usuário através do dashboard
    Author:    Rafael Briet <rafael-briet@hotmail.com>

---
+++ dashboard/usuarios/create.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = {
+                    dados_pessoais: {
+                    nome: form.elements.userName.value,
+                    sobrenome: form.elements.userSurname.value,
+                    data_nascimento: form.elements.userBirthday.value,
+                    biografia: form.elements.userBio.value,
+                    nome_foto: ""
+                },
+                    usuario: {
+                    email: form.elements.userEmail.value,
+                    senha: form.elements.userEmail.value + Date(),
+                    termos_condicoes: 1,
+                    id_nivel_acesso: form.elements.userAccessLevel.value
+                }
+    };
+
+    try {
+        const token = document.cookie.split(';')[0].split('=')[1];
+        const response = await fetch('http://localhost/equiztech/api/usuarios/', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': token,
+            },
+            body: JSON.stringify(data)
+        });
+        const result = await response.json();
+
+        if (result.erro != null) {
+            document.querySelector('#userEmail-duplicate').classList.remove('d-none');
+        } else {
+            document.querySelector('#userEmail-duplicate').classList.add('d-none');
+            form.reset();
+            const toastElement = document.querySelector('#form-success');
+            const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastElement);
+            toastBootstrap.show();
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+});
--- dashboard/usuarios/create.php
+++ dashboard/usuarios/create.php
@@ -6,6 +6,16 @@ only_admin_allowed();
 get_header();
 get_partial('nav');
 
+$url = $_ENV['BASE_URL'] . '/api/usuarios/nivel-acesso.php/';
+$curl_handler = curl_init($url);
+curl_setopt($curl_handler, CURLOPT_HTTPGET, true);
+curl_setopt($curl_handler, CURLOPT_RETURNTRANSFER, true);
+$authorization = 'Authorization: ' . $_COOKIE['TOKEN'];
+curl_setopt($curl_handler, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
+$response_json = curl_exec($curl_handler);
+curl_close($curl_handler);
+$access_level = json_decode($response_json, true);
+
 ?>
 
 <div class="container-fluid">
@@ -31,8 +41,9 @@ get_partial('nav');
                         <label for="userAccessLevel" class="form-label">Nível de acesso</label>
                         <select class="form-select" id="userAccessLevel">
                             <option value="0" selected disabled hidden>Selecione um nível de acesso</option>
-                            <option value="volvo">Administrador</option>
-                            <option value="saab">Jogador</option>
+                            <?php foreach ($access_level['nivel_acesso'] as $access) : ?>
+                                <option value="<?php echo $access['id']; ?>"><?php echo $access['nome']; ?></option>
+                            <?php endforeach; ?>
                         </select>
                     </div>
                     <div class="mb-3">
@@ -46,6 +57,7 @@ get_partial('nav');
                     <div class="mb-3">
                         <label for="userEmail" class="form-label">Email</label>
                         <input type="email" class="form-control" id="userEmail" required>
+                        <p class="form-text text-danger d-none" id="userEmail-duplicate">Já existe um usuário cadastrado com esse email.</p>
                     </div>
                     <div class="d-flex gap-2 justify-content-end">
                         <a href="../usuarios" class="btn btn-outline-secondary">Voltar</a>
@@ -57,13 +69,14 @@ get_partial('nav');
                     <div class="toast align-items-center border border-success-subtle bg-success-subtle text-success" id="form-success" role="alert" aria-live="assertive" aria-atomic="true">
                         <div class="d-flex">
                             <div class="toast-body">
-                                <p>Categoria criada com sucesso.</p>
+                                <p>Usuário criado com sucesso.</p>
                             </div>
                             <button type="button" class="btn-close me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
                         </div>
                     </div>
                 </div>
             </div>
+            <script src="<?php echo $_ENV['BASE_URL']; ?>/dashboard/usuarios/create.js"></script>
         </main>
     </div>
 </div>
