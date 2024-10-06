0b1a2824001f31c4e92f0aff4b208322f9d047c1
Rafael Briet <rafael-briet@hotmail.com>
2024-10-06 00:50:12 -0300

    Adiciona link para voltar a tela inicial do dashboard

--- dashboard/partials/nav.php
+++ dashboard/partials/nav.php
@@ -1,6 +1,6 @@
 <nav class="navbar bg-light-subtle sticky-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="<?php echo $_ENV['BASE_URL'] ?>/dashboard">
             <img src="<?php get_image('logo_equiztech.svg'); ?>" alt="Logo Equiztech" height="32">
         </a>
         <button class="navbar-toggler d-md-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarMenu" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
