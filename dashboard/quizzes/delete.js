5c6053356ee139647a868940a21e06af22c4b3e7
Rafael Briet <rafael-briet@hotmail.com>
2024-10-05 23:59:20 -0300

    Adicionar página para deletar uma pergunta e suas alternativas

---
+++ dashboard/quizzes/delete.js
@@ -0,0 +1,30 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    try {
+        const token = Cookies.get('TOKEN');
+        const response = await fetch(CONFIG.base_url + '/api/perguntas/?id=' + form.elements.userId.value, {
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': token,
+            },
+        });
+        const result = await response.json();
+
+        if (result.erro != null) {
+            console.error(result.erro);
+            // document.querySelector('#categoryName-duplicate').classList.remove('d-none');
+        } else {
+            // document.querySelector('#categoryName-duplicate').classList.add('d-none');
+            const toastElement = document.querySelector('#form-success');
+            const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastElement);
+            toastBootstrap.show();
+            window.location.href = '../';
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    }
+});
